223336cc610741c216a72474ab66b1c36b19e31b
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-06 12:40:25 +0100

    Initial commit

---
+++ src/main/java/com/http/td/HttpExemplesApplication.java
@@ -0,0 +1,13 @@
+package com.http.td;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpExemplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpExemplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/http/td/HttpExemplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.http.td;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpExemplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
